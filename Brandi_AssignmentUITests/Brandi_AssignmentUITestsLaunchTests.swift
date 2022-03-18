6001a601471499145f1276d3ed5b2e94283eab97
AlexHwang <cksgh0316@gmail.com>
2022-03-18 13:35:57 +0900

    Initial Commit

---
+++ Brandi_AssignmentUITests/Brandi_AssignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Brandi_AssignmentUITestsLaunchTests.swift
+//  Brandi_AssignmentUITests
+//
+//  Created by AlexHwang on 2022/03/18.
+//
+
+import XCTest
+
+class Brandi_AssignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
